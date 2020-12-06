958dc0311dea764d508675fb0aba42484f90c73f
jfridgen <jfridgen@gmail.com>
2020-12-05 20:53:52 -0600

    Finish Day 5 Part 2

--- day-5.js
+++ day-5.js
@@ -20,16 +20,28 @@ function findRowOrSeat(choices, seatIdPart, lowerInd, upperInd) {
   }
 
   if (seatIdPart.substring(0, 1) == lowerInd) {
-    return findRowOrSeat(choices.slice(0, choices.length / 2), seatIdPart.substring(1), lowerInd, upperInd);
+    return findRowOrSeat(choices.slice(0, choices.length / 2),
+      seatIdPart.substring(1), lowerInd, upperInd);
   }
 
   if (seatIdPart.substring(0, 1) == upperInd) {
-    return findRowOrSeat(choices.slice(choices.length / 2, choices.length), seatIdPart.substring(1), lowerInd, upperInd);
+    return findRowOrSeat(choices.slice(choices.length / 2, choices.length),
+      seatIdPart.substring(1), lowerInd, upperInd);
   }
 }
 
 const rows = getArray(128);
 const seats = getArray(8);
+
+function getAllPossibleSeats() {
+  let allPossibleSeats = [];
+  for (row of rows) {
+    for (seat of seats) {
+      allPossibleSeats.push((row * 8) + seat);
+    }
+  }
+  return allPossibleSeats;
+}
 
 function getSeatNumber(seatId) {
   const row = findRowOrSeat(rows, seatId.substring(0, 7), "F", "B");
@@ -37,7 +49,19 @@ function getSeatNumber(seatId) {
   return (row * 8) + seat;
 }
 
+function actualOpenSeat(seat, openSeats) {
+  return (openSeats.indexOf(seat) == -1) &&
+    (openSeats.indexOf(seat - 1) != -1) &&
+    (openSeats.indexOf(seat + 1) != -1);
+}
+
+function getOpenSeats(allPossibleSeats, openSeats) {
+  return allPossibleSeats.filter(seat => actualOpenSeat(seat, openSeats));
+}
 
 const inputLines = readInputLines("./day-5-input.txt");
+const allPossibleSeats = getAllPossibleSeats();
+const takenSeats = inputLines.map(line => getSeatNumber(line));
+const openSeats = getOpenSeats(allPossibleSeats, takenSeats);
 
-console.log(inputLines.map(line => getSeatNumber(line)).reduce(function (a, b) { return a > b ? a : b }));
+console.log(openSeats);
