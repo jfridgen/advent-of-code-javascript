b0723131ad688b04b73552482a5fda04cb443ce1
jfridgen <jfridgen@gmail.com>
2020-12-07 16:22:45 -0600

    Finish Day 6 Part 2

--- day-6.js
+++ day-6.js
@@ -1,17 +1,34 @@
-function readInputSplitByBlankLines(filePath) {
+function readInputByGroup(filePath) {
   const fs = require("fs"); // Built-in Node.js module
   const text = fs.readFileSync(filePath).toString("utf-8");
-  const textSplitByBlankLine = text.split("\r\n\r\n").map(line => line.replace(/\r\n/g, ""));
+  //const textSplitByBlankLine = text.split("\r\n\r\n").map(line => line.replace(/\r\n/g, ""));
+  const textSplitByBlankLine = text.split("\r\n\r\n");
 
   return textSplitByBlankLine;
+}
+
+function getUnanimousGroupYesResponses(responses) {
+  const individualResponses = responses.split("\r\n");
+  const unanimousResponses = [];
+  let chars;
+  for(response of individualResponses) {
+    chars = response.split("");
+    for(char of chars) {
+      if(individualResponses.every(r => r.includes(char))) {
+        unanimousResponses.push(char);
+      }
+    }
+  }
+
+  return [...new Set(unanimousResponses)];
 }
 
 function getUniqueResponses(responses) {
   return [...new Set(responses)];
 }
 
-const groups = readInputSplitByBlankLines("./day-6-input.txt");
-const groupResponses = groups.map(line => line.split(""));
-const groupUniqueResponses = groupResponses.map(getUniqueResponses);
-const totalUniqueResponses = groupUniqueResponses.reduce((acc, cur) => acc + cur.length, 0);
-console.log(totalUniqueResponses);
+const groups = readInputByGroup("./day-6-input.txt");
+const unanimousResponses = groups.map(getUnanimousGroupYesResponses);
+const total = unanimousResponses.reduce((acc, cur) => acc + cur.length, 0);
+
+console.log(total);
