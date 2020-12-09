240a7cf7dea7092118810467cbdb1f108ff01d3b
jfridgen <jfridgen@gmail.com>
2020-12-09 15:10:41 -0600

    Finish Day 9 Part 2

--- day-9.js
+++ day-9.js
@@ -6,20 +6,46 @@ function readInputLines(filePath) {
   return textByLine;
 }
 
-function twoNumsSum(numToCheck, list) {
-  for(let num of list) {
-    if(list.filter(n => n == (numToCheck - num)).length == 1) {
+function twoNumsSum(numToCheck, allNums) {
+  for(let num of allNums) {
+    if(allNums.filter(n => n == (numToCheck - num)).length == 1) {
       return true;
     }
   }
   return false;
 }
 
-const allNums = readInputLines("./day-9-input.txt");
-const preambleLength = 25;
+function getInvalidNum(allNums, preambleLength) {
+  let invalidNum = 0;
+  for(let i = preambleLength; i < allNums.length; ++i) {
+    if(!twoNumsSum(allNums[i], allNums.slice(i - preambleLength, i))) {
+      invalidNum = allNums[i];
+    }
+  }
+  return invalidNum;
+}
 
-for (let i = preambleLength; i < allNums.length; ++i) {
-  if(!twoNumsSum(allNums[i], allNums.slice(i - preambleLength, i))) {
-    console.log(allNums[i]);
+function getBlockThatSumsTo(numToCheck, allNums) {
+  let sum;
+  outerloop:
+  for(let i = 0; i < allNums.length; ++i) {
+    sum = allNums[i];
+    for(let j = i + 1; j < allNums.length; ++j) {
+      sum += allNums[j];
+      if(sum == numToCheck) {
+        return allNums.slice(i, j + 1);
+      }
+      if(sum > numToCheck) {
+        break;
+      }
+    }
   }
 }
+
+const allNums = readInputLines("./day-9-input.txt").map(line => parseInt(line));
+const preambleLength = 25;
+const invalidNum = getInvalidNum(allNums, preambleLength);
+const block = getBlockThatSumsTo(invalidNum, allNums);
+const result = Math.min(...block) + Math.max(...block);
+
+console.log(result);
