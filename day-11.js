409e2a587e0094a9c1e2defed1e7baefa94d66cb
jfridgen <jfridgen@gmail.com>
2020-12-11 17:30:04 -0600

    Save partially done Day 11 Part 2

--- day-11.js
+++ day-11.js
@@ -8,22 +8,65 @@ function readInputLines(filePath) {
 
 const EMPTY = "L";
 const OCCUPIED = "#";
+const NORTH = "N";
+const SOUTH = "S";
+const EAST = "E";
+const WEST = "W";
 
-function numOccupiedAdjacentSeats(seatsByRow, currentRow, currentSeat) {
-  let num = 0;
-  let startingRow = (currentRow == 0) ? 0 : currentRow - 1;
-  let endingRow = (currentRow >= seatsByRow.length - 1) ? currentRow : currentRow + 1;
-  let startingSeat = (currentSeat == 0) ? 0 : currentSeat - 1;
-  let endingSeat = (currentSeat >= seatsByRow[currentRow].length - 1) ? currentSeat : currentSeat + 1;
+function getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, northSouth, eastWest) {
+  let row;
+  if(northSouth == NORTH) {
+    row = currentRow - 1;
+  } else if(northSouth == SOUTH) {
+    row = currentRow + 1;
+  }
 
-  for(let row = startingRow; row <= endingRow; ++row) {
-    for(let seat = startingSeat; seat <= endingSeat; ++seat) {
-      if((!(row == currentRow && seat == currentSeat)) && seatsByRow[row][seat] == OCCUPIED) {
-        num += 1;
-      }
+  let seat;
+  if(eastWest == WEST) {
+    seat = currentSeat - 1;
+  } else if(eastWest == EAST) {
+    seat = currentSeat + 1;
+  }
+
+  let count = 0;
+  while(row >= 0 && row < seatsByRow.length &&
+    seat >= 0 && seat < seatsByRow[row].length) {
+    if(seatsByRow[row][seat] == OCCUPIED) {
+      count += 1;
+      break;
+    }
+    if(seatsByRow[row][seat] == EMPTY) {
+      break;
+    }
+
+    if(northSouth == NORTH) {
+      ++row;
+    } else if(northSouth == SOUTH) {
+      --row;
+    }
+
+    if(eastWest == WEST) {
+      --seat;
+    } else if(eastWest == EAST) {
+      ++seat;
     }
   }
-  return num;
+
+  return count;
+}
+
+function getAllOccupiedVisibleSeats(seatsByRow, currentRow, currentSeat) {
+  const westCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, null, WEST);
+  const northWestCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, NORTH, WEST);
+  const northCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, NORTH, null);
+  const northEastCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, NORTH, EAST);
+  const eastCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, null, EAST);
+  const southEastCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, SOUTH, EAST);
+  const southCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, SOUTH, null);
+  const southWestCount = getOccupiedVisibleSeatsForDirection(seatsByRow, currentRow, currentSeat, SOUTH, WEST);
+
+  return westCount + northWestCount + northCount + northEastCount +
+    eastCount + southEastCount + southCount + southWestCount;
 }
 
 function applyRules(seatsByRow) {
@@ -31,9 +74,9 @@ function applyRules(seatsByRow) {
 
   for(let row = 0; row < seatsByRow.length; ++row) {
     for(let seat = 0; seat < seatsByRow[row].length; ++seat) {
-      if(seatsByRow[row][seat] == EMPTY && numOccupiedAdjacentSeats(seatsByRow, row, seat) == 0) {
+      if(seatsByRow[row][seat] == EMPTY && getAllOccupiedVisibleSeats(seatsByRow, row, seat) == 0) {
         updated[row][seat] = OCCUPIED;
-      } if(seatsByRow[row][seat] == OCCUPIED && numOccupiedAdjacentSeats(seatsByRow, row, seat) >= 4) {
+      } if(seatsByRow[row][seat] == OCCUPIED && getAllOccupiedVisibleSeats(seatsByRow, row, seat) >= 5) {
         updated[row][seat] = EMPTY;
       }
     }
@@ -55,6 +98,7 @@ function arrayEquals(a, b) {
 
 function applyRulesUntilNoChange(seatsByRow) {
   let result = applyRules(seatsByRow);
+  console.log(result);
 
   if(!arrayEquals(result, seatsByRow)) {
     return applyRulesUntilNoChange(result);
